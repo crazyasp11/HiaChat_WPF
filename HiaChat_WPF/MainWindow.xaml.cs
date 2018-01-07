ebd84604cc6c0a56bedd12fe48a8c0c83d3603e8
陈振宇 <897848522@qq.com>
2018-01-07 23:19:17 +0800

    数据库通过连接

--- HiaChat_WPF/MainWindow.xaml.cs
+++ HiaChat_WPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Npgsql;
 
 namespace HiaChat_WPF
 {
@@ -23,6 +24,29 @@ namespace HiaChat_WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //NpgsqlConnection connection = new NpgsqlConnection("Server = 192.168.44.10; Port = 5432; UserId = HiaChat; Password = hiahia;Database = HiaChatDB");
+            //NpgsqlConnection connection = new NpgsqlConnection("Server = localhost; Port = 2222; UserId = HiaChat; Password = hiahia;Database = HiaChatDB");
+            NpgsqlConnection connection = new NpgsqlConnection("Server = 192.168.1.109; Port = 2222; UserId = HiaChat; Password = hiahia;Database = HiaChatDB");
+
+            connection.Open();
+            string sqlCommand = "SELECT nick_name FROM public.basic_user_inf where id = 1;";
+            NpgsqlCommand command = new NpgsqlCommand(sqlCommand, connection);
+            NpgsqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+            string data;
+            data = reader.GetString(0);
+
+            label.Content = data;
+            connection.Close();
+
+
+            //仅执行SQL语句
+            //command.ExecuteNonQuery();
+
         }
     }
 }
